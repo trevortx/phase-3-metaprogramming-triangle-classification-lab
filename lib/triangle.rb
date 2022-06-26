4d53fe5860834998b9dd9f7d71fc0f999ae833e7
Trevor Vardeman <trevortx@gmail.com>
2022-06-26 00:41:16 -0700

    commit tree 1c237b58eef7e5a32ce528b4c1f3569183c762f7

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  def initialize(l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+
+  def kind
+    sides = [@l1, @l2, @l3].sort
+    unique_sides = sides.uniq
+    if @l1.ceil | @l2.ceil | @l3.ceil <= 0
+      raise TriangleError
+    elsif sides[0] + sides[1] <= sides[2]
+      raise TriangleError
+    elsif unique_sides.length == 3
+      :scalene
+    elsif unique_sides.length == 2
+      :isosceles
+    else unique_sides.length == 1
+      :equilateral
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "invalid triangle"
+    end
+  end
 end
